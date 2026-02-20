8262ab66a54c1f5cc5739a542471f766f6ab6257
Alexandre3000z <joaoalexandrems3000@gmail.com>
2026-02-20 16:50:18 -0300

    Criando base para funcionalidade telegram

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+import os
+
+# Buscando Token do arquivo .env
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+# Criando Função para o comando /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🚀 Olá! Eu sou seu bot!")
+
+# Criando Função para o comando /bomdia
+async def bomdia(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🌞 Bom dia! Que seu dia seja produtivo!")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("bomdia", bomdia))
+
+app.run_polling()
